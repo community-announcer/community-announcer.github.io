8612ef51ddd59b921227c42bd651da87dbbf49d9
Burak Ince <burak.ince@linux.org.tr>
2018-05-10 23:56:47 +0200

    Add Auth0 functionality

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import { makeMainRoutes } from './routes';
 import registerServiceWorker from './registerServiceWorker';
 
+const routes = makeMainRoutes();
+
 ReactDOM.render(
-  <App />,
+  routes,
   document.getElementById('root')
 );
+
 registerServiceWorker();
